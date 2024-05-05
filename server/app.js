5a75dc9007b441036d496dcefb03f4dc9ffc7d2e
Marty-Dev <velek.martin@email.cz>
2024-05-05 16:23:23 +0200

    Minor changes

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const app = express()
 const port = 3000
 
 const categoryController = require('./controller/category.js')
+const incomeController = require('./controller/income.js')
+const expenseController = require('./controller/expense.js')
+const targetController = require('./controller/target.js')
 
 app.use(express.json())  // support for application/json
 app.use(express.urlencoded({ extended: true }))  // support for application/x-www-form-urlencoded
@@ -11,10 +14,9 @@ app.use(express.urlencoded({ extended: true }))  // support for application/x-ww
 app.use(cors())
 
 app.use('/category', categoryController)
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the server!')
-})
+app.use('/income', incomeController)
+app.use('/expense', expenseController)
+app.use('/target', targetController)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
